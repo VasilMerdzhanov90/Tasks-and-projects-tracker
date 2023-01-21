fd0f986ace1ffa781bb02bde4f601551c5b19e4c
Vasil Merdzhanov <vasilmerdjanov1@gmail.com>
2023-01-21 13:05:44 +0200

    correction
    fixed bug: not loggin out!

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -17,23 +17,24 @@ export const authReducer = (state, action) => {
 }
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { 
+  const [state, dispatch] = useReducer(authReducer, {
     user: null,
     authIsReady: false
   })
 
   useEffect(() => {
     const unsub = projectAuth.onAuthStateChanged(user => {
-      dispatch({ type: 'AUTH_IS_READY', payload: user })
+      //payload last value was user!!!!!!!!!
+      dispatch({ type: 'AUTH_IS_READY', payload: null })
       unsub()
     })
   }, [])
 
   console.log('AuthContext state:', state)
-  
+
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
-      { children }
+      {children}
     </AuthContext.Provider>
   )
 
